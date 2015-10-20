4925f07aa245a3e3f3642349745dea2345fb84e7
Ross Kitsis <ross.kitsis@gmail.com>
2015-10-20 14:55:12 -0400

    Added examples and started vanet building

---
+++ MySimpleVanet.h
@@ -0,0 +1,17 @@
+/*
+ * MySimpleVanet.h
+ *
+ *  Created on: Oct 20, 2015
+ *      Author: ross
+ */
+
+#ifndef SCRATCH_MYSIMPLEVANET_H_
+#define SCRATCH_MYSIMPLEVANET_H_
+
+class MySimpleVanet {
+public:
+	MySimpleVanet();
+	virtual ~MySimpleVanet();
+};
+
+#endif /* SCRATCH_MYSIMPLEVANET_H_ */
